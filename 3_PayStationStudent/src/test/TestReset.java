588ea453baf8821849fed004a1a03db251d83183
ManifestManah <ManifestManah@users.noreply.github.com>
2025-02-24 09:58:16 +0100

    Added: Supplied Education Files
    Added the files that were supplied by the educator for the miniprojekt test project.

---
+++ 3_PayStationStudent/src/test/TestReset.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.*;
+import org.junit.jupiter.api.BeforeAll;
+
+import controllayer.*;
+import modellayer.*;
+
+/**
+ * Inspired by the book: Flexible, Reliable Software Henrik B�rbak Christensen:
+ * Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
+ */
+
+public class TestReset {
+
+	ControlPayStation ps;
+
+	/** Fixture for pay station testing. */
+	@BeforeAll
+	public void setUp() {
+		ps = new ControlPayStation();
+	}
+
+	/**
+	 * Verify that the pay station is cleared and display shows 0 after a buy scenario
+	 */
+	@Test
+	public void shouldClearAfterBuy() throws IllegalCoinException, Exception {
+		//
+	}
+
+	/**
+	 * Verify that cancel() clears the pay station
+	 */
+	@Test
+	public void shouldClearAfterCancel() throws IllegalCoinException {
+		//
+	}
+}
